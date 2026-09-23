fdb4c935abc68dae1abaf37d076cacd33fc938c2
AnDoanx <168345402+AnDoanx@users.noreply.github.com>
2026-09-23 09:05:08 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// four pivots, ONE point in
-// space - a hand of cards,
-// not four tilted boxes
-slot.animate([
-  { transform: `translate(${dx[i]}px) rotate(${ANG[i]}deg)` }
-], {
-  duration: 560,
-  delay: i * 45
-});
